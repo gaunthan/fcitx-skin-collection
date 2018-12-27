d5f7c842151ef2d717aab912c9cb91701b1c108b
gaunthan <gaunthanhuang@foxmail.com>
2018-12-27 10:30:05 +0800

    In case that repo already exist.

--- install-fcitx-skins.sh
+++ install-fcitx-skins.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
+
+TARGET=/tmp/fcitx-skin-collection
+
 cd /tmp
-git clone https://github.com/gaunthan/fcitx-skin-collection.git
+if [ ! -d $TARGET ]; then
+    git clone https://github.com/gaunthan/fcitx-skin-collection.git
+fi
+
 mkdir ~/.config/fcitx/skin/ -p
-cp fcitx-skin-collection/skin/* ~/.config/fcitx/skin/ -r
+cp $TARGET/skin/* ~/.config/fcitx/skin/ -r
 
 echo "Now you can right click input keyboard on system tray, "
 echo "and you will see Skin item. Choose one :), and enjoy it."
